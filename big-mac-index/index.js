3f2038aa501663e43ea3515fcb87b6f320ae428b
Rafael Armelin <raffael.armelin@gmail.com>
2021-07-12 18:20:33 +0100

    finished

--- big-mac-index/index.js
+++ big-mac-index/index.js
@@ -18,3 +18,41 @@ For example, using figures in July 2008:
   - (2.00 – 1.56) / 1.56 = 28%
   - the pound was thus overvalued against the dollar by 28%
 */
+
+const usBigMac = 3.99;
+const ukBigMac = 3.19;
+const baseCurrencyVsGbpRate = 1.39;
+const usUkPpp = usBigMac/ukBigMac;
+const tradingPercentageUk = (baseCurrencyVsGbpRate-usUkPpp)/usUkPpp*100;
+
+console.log(tradingPercentageUk.toFixed(1) + "%");
+
+const brazilBigMac = 19.10;
+const baseCurrencyVsBrazilianRealRate = 0.19;
+const usBrPpp = usBigMac/brazilBigMac;
+const tradingPercentageBr = (baseCurrencyVsBrazilianRealRate-usBrPpp)/usBrPpp*100;
+
+console.log(tradingPercentageBr.toFixed(1)+"%");
+
+const italyBigMac = 4.89;
+const baseCurrencyVsEuroRate = 1.19;
+const usItalyPpp = usBigMac/italyBigMac;
+const tradingPercentageEuro = (baseCurrencyVsEuroRate-usItalyPpp)/usItalyPpp*100;
+
+console.log(tradingPercentageEuro.toFixed(1)+"%");
+
+
+const kosovoBigMac = 3.49;
+const usKosovoPpp = usBigMac/kosovoBigMac;
+const tradingPercentageEuroKosovoRate = (baseCurrencyVsEuroRate-usKosovoPpp)/usKosovoPpp*100;
+
+console.log(tradingPercentageEuroKosovoRate.toFixed(1)+"%");
+
+const hongKongBigMac = 2.64;
+const baseCurrencyVsHongKongDollarRate = 0.13;
+const usHongKongPpp = usBigMac/hongKongBigMac;
+const tradingPercentageHongKongDollarRate = (baseCurrencyVsHongKongDollarRate-usHongKongPpp)/usHongKongPpp*100;
+
+console.log(tradingPercentageHongKongDollarRate.toFixed(1)+"%");
+
+
